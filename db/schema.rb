aea7815ce49dbea145b05ae8fdff47ddb800be64
Ian Muisyo Ngila <ngilaian@gmail.com>
2022-11-02 14:58:40 +0300

    resolving conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -44,7 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_31_060900) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
++                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  ++++++
   create_table "managers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
